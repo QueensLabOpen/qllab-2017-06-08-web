2fb16583b1ac24dfca3fe712f212c460c0dadf97
Henrik Ruscon <henrik.dahlheim@gmail.com>
2017-06-08 21:50:41 +0200

    add header component

--- resources/assets/js/components/App.vue
+++ resources/assets/js/components/App.vue
@@ -1,10 +1,13 @@
 <template>
     <div id="app">
+        <app-header></app-header>
         <router-view></router-view>
     </div>
 </template>
 
 <script>
+import AppHeader from '../components/Header';
+
 export default {
     watch: {
         title(value) {
@@ -16,6 +19,10 @@ export default {
         title() {
             return this.$store.state.title;
         }
+    },
+
+    components: {
+        AppHeader
     }
 }
 </script>
---
+++ resources/assets/js/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+    <header class="header" v-if="user">
+        <div class="header__item">
+            <router-link to="/" class="header__logo" exact></router-link>
+        </div>
+        <div class="header__item">
+            <a class="item__link--logout h-interaction-clickable" @click.prevent="logout">Logout</a>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    computed: {
+        user() {
+            return this.$store.state.user;
+        }
+    },
+
+    methods: {
+        logout() {
+            axios.post('/logout')
+            .then(({ data }) => {
+                this.$store.dispatch('LOGOUT', data.csrfToken);
+                this.$router.push({ name: 'login' });
+            });
+        }
+    }
+}
+</script>
